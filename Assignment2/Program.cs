1729eef9a13e42b08a35ad4a1967819a8b478b82
Ahmed Hesham <ah0115915v@gmail.com>
2026-07-25 13:07:08 +0300

    Question2

--- Assignment2/Program.cs
+++ Assignment2/Program.cs
@@ -11,6 +11,29 @@
             //Console.WriteLine("title: " + title);
             //Console.WriteLine("upperTitle: " + upperTitle);
             #endregion
+
+
+
+
+            #region Question2
+            //string a = "Clean Code";
+            //string b = "Clean Code";
+
+            //bool sameObject = ReferenceEquals(a, b);
+            //Console.WriteLine("ReferenceEquals(a, b): " + sameObject);
+            #endregion
+
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
